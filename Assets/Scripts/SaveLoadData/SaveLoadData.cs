cd48277f87398c38c38a6b6eacc38aba15b6865f
MrWocher <kotim4444@gmail.com>
2024-09-30 16:22:10 +0700

    Save Load Currency

--- Assets/Scripts/SaveLoadData/SaveLoadData.cs
+++ Assets/Scripts/SaveLoadData/SaveLoadData.cs
@@ -73,5 +73,121 @@ namespace SurvivalChicken.SaveLoadDatas
         }
 
         #endregion
+
+        #region Currency Values
+
+        public enum CurrencyTypes
+        {
+            Coins,
+            Gems,
+            Energy
+        }
+
+        public void SaveCurrencyValue(int value, CurrencyTypes currencyType)
+        {
+            string path = GeneralPath + "CurrencyValues.txt";
+
+            string coinsLabel = "Coins";
+            string gemsLabel = "Gems";
+            string energyLabel = "Energy";
+
+            List<string> lines = new List<string>();
+            Dictionary<string, int> currencies = new Dictionary<string, int>();
+
+            using(FileStream file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read, FileShare.None))
+            using(StreamReader streamReader = new StreamReader(file))
+            {
+                while(streamReader.Peek() > -1)
+                    lines.Add(streamReader.ReadLine());
+
+                foreach(string line in lines)
+                {
+                    string[] splitStr = line.Split(' ');
+                    currencies.Add(splitStr[0], System.Convert.ToInt32(splitStr[1]));
+                }
+            }
+
+            switch (currencyType)
+            {
+                case CurrencyTypes.Coins:
+                    if (currencies.ContainsKey(coinsLabel))
+                        currencies[coinsLabel] = value;
+                    break;
+                case CurrencyTypes.Gems:
+                    if (currencies.ContainsKey(gemsLabel))
+                        currencies[gemsLabel] = value;
+                    break;
+                case CurrencyTypes.Energy:
+                    if (currencies.ContainsKey(energyLabel))
+                        currencies[energyLabel] = value;
+                    break;
+            }
+
+            using(StreamWriter streamWriter = new StreamWriter(path, false))
+            {
+                streamWriter.Write("");
+            }
+
+            using(StreamWriter streamWriter = new StreamWriter(path, true))
+            {
+                foreach (string str in currencies.Keys)
+                    streamWriter.WriteLine($"{str} {currencies[str]}");
+            }
+        }
+
+        public bool TryGetCurrcyValue(out int value, CurrencyTypes currencyType)
+        {
+            string path = GeneralPath + "CurrencyValues.txt";
+
+            string coinsLabel = "Coins";
+            string gemsLabel = "Gems";
+            string energyLabel = "Energy";
+
+            List<string> lines = new List<string>();
+            Dictionary<string, int> currencies = new Dictionary<string, int>();
+
+            using (FileStream file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read, FileShare.None))
+            using (StreamReader streamReader = new StreamReader(file))
+            {
+                while (streamReader.Peek() > -1)
+                    lines.Add(streamReader.ReadLine());
+
+                foreach (string line in lines)
+                {
+                    string[] splitStr = line.Split(' ');
+                    currencies.Add(splitStr[0], System.Convert.ToInt32(splitStr[1]));
+                }
+            }
+
+            switch (currencyType)
+            {
+                case CurrencyTypes.Coins:
+                    if (currencies.ContainsKey(coinsLabel))
+                    {
+                        value = currencies[coinsLabel];
+                        return true;
+                    }
+                    break;
+                case CurrencyTypes.Gems:
+                    if (currencies.ContainsKey(gemsLabel))
+                    {
+                        value = currencies[gemsLabel];
+                        return true;
+                    }
+                    break;
+                case CurrencyTypes.Energy:
+                    if (currencies.ContainsKey(energyLabel))
+                    {
+                        value = currencies[energyLabel];
+                        return true;
+                    }
+                    break;
+            }
+
+            value = -1;
+            return false;
+        }
+
+        #endregion
     }
 }
